112b6a7abb6aeb3906d4303d04926d13d98edcaf
carcarrillogo <caarcarrillogo@ittepic.edu.mx>
2018-10-10 08:48:52 -0600

    first commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component ({
+  selector: 'page-html',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  constructor (public navCtrl: NavController){
+    
+  }
+}
